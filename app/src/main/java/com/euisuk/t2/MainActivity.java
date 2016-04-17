9c939976ffdd317b29ca4216e033f96e499de81f
ES <euisuk@euisuk.com>
2016-04-16 21:12:34 -0700

    ch6

--- app/src/main/java/com/euisuk/t2/MainActivity.java
+++ app/src/main/java/com/euisuk/t2/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         // ES : ch3
         // ES: ch4 in Server
         // ES: ch5 on LOCAL
+        // ES: ch6 in Local br1
 
         return super.onOptionsItemSelected(item);
     }
